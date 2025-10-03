aa173e69ef075adc1f28648c402ed20c315cf56d
Pooja-154 <poojabellapu@gmail.com>
2025-10-03 23:52:58 +0530

    Added base config for GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/dr.nopain/" : "/", 
+  base: mode === "production" ? "/DrNoPainDental/" : "/", 
   server: {
     host: "::", 
     port: 8080,
